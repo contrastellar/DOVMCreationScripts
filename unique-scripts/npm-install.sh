0cdca712c255cbc47e87d3984bc5244fa0ef548e
Gabriella Agathon <gjstimac@mtu.edu>
2022-02-28 14:05:15 -0500

    Updated fish, npm install scripts. Added monitoring agent install script.

--- unique-scripts/npm-install.sh
+++ unique-scripts/npm-install.sh
@@ -3,7 +3,7 @@ echo "Installing NPM. This will require giving sudo permissions"
 sudo -v #This simply validates the user
 
 #Command can now be safely run
-sudo apt-get install npm
+sudo apt-get -q install npm
 
 echo "NPM should be installed. Version below..."
 npm -v
